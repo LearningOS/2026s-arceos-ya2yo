f04504a12f71d6fa0a19005a256492e1ff370461
Ya2yo <yayoy0015@gmail.com>
2026-04-18 16:08:05 +0800

    sys_mmap

--- arceos/exercises/sys_map/src/syscall.rs
+++ arceos/exercises/sys_map/src/syscall.rs
@@ -8,6 +8,7 @@ use axtask::current;
 use axtask::TaskExtRef;
 use axhal::paging::MappingFlags;
 use arceos_posix_api as api;
+use memory_addr::{PAGE_SIZE_4K, VirtAddrRange};
 
 const SYS_IOCTL: usize = 29;
 const SYS_OPENAT: usize = 56;
@@ -26,17 +27,33 @@ const AT_FDCWD: i32 = -100;
 ///
 /// It will receive a function which return Result<_, LinuxError> and convert it to
 /// the type which is specified by the caller.
+/// 该宏接收两个参数：
+/// 1. $fn: 函数标识符（用于日志打印函数名）
+/// 2. $stmt: 具体业务代码块（系统调用的核心实现逻辑）
 #[macro_export]
 macro_rules! syscall_body {
     ($fn: ident, $($stmt: tt)*) => {{
+        // 1. 执行具体的业务逻辑块
+        // 这里使用了一个“立即执行闭包 (IIFE)”，并将返回类型强制指定为 axerrno::LinuxResult<_>。
+        // 这样做的好处是：在 $($stmt)* 代码块内部可以直接使用 `?` 操作符来处理错误。
         #[allow(clippy::redundant_closure_call)]
         let res = (|| -> axerrno::LinuxResult<_> { $($stmt)* })();
+         // 2. 自动日志打印
+        // 根据执行结果 res 分级打印日志，方便调试。
         match res {
+            // 如果成功 (Ok) 或者返回 EAGAIN (资源暂时不可用，通常在非阻塞 IO 中常见，不视为严重错误)
+            // 使用 debug! 级别打印，格式类似于 "sys_read => Ok(10)"
             Ok(_) | Err(axerrno::LinuxError::EAGAIN) => debug!(concat!(stringify!($fn), " => {:?}"),  res),
+            // 如果发生了真正的错误 (如 EPERM, ENOENT 等)
+            // 使用 info! 级别打印，以便开发者在控制台更容易注意到失败的系统调用
             Err(_) => info!(concat!(stringify!($fn), " => {:?}"), res),
         }
+        // 3. 返回值转换 (符合 Linux ABI 规范)
+        // Linux 系统调用约定：成功返回正数或 0，失败返回负的错误码（-Errno）。
         match res {
             Ok(v) => v as _,
+            // 失败时：获取错误对象 e 对应的错误码数字（如 EBADF 是 9），
+            // 然后取其负数（-9）并转换类型返回。
             Err(e) => {
                 -e.code() as _
             }
@@ -140,7 +157,35 @@ fn sys_mmap(
     fd: i32,
     _offset: isize,
 ) -> isize {
-    unimplemented!("no sys_mmap!");
+    syscall_body!(sys_mmap,{
+        if length ==0 {
+            return Err(LinuxError::EINVAL);
+        }
+        let length=(length+PAGE_SIZE_4K-1)&!(PAGE_SIZE_4K-1);
+        let prot=MmapProt::from_bits(prot).ok_or(LinuxError::EINVAL)?;
+        let flags=MmapFlags::from_bits(flags).ok_or(LinuxError::EINVAL)?;
+        let mut mapping_flags=prot.into();
+        mapping_flags|=MappingFlags::WRITE;
+        let cur=current();
+        let mut aspace=cur.task_ext().aspace.lock();
+        let mut start_vddr=addr as usize;
+        if flags.contains(MmapFlags::MAP_FIXED)&&start_vddr%PAGE_SIZE_4K!=0 {
+            return Err(LinuxError::EINVAL);
+        }
+
+        if !flags.contains(MmapFlags::MAP_FIXED)&&start_vddr==0{
+            start_vddr=aspace.find_free_area(0x2000_0000.into(),length,VirtAddrRange::new(0x2000_0000.into(), usize::MAX.into())).unwrap().as_usize();
+        }
+        aspace.map_alloc(start_vddr.into(), length, mapping_flags, true)?;
+        drop(aspace);
+        if fd!=-1 {
+            let read_len=api::sys_read(fd, start_vddr as *mut c_void, length);
+            if read_len<0{
+                debug!("mmap file read failed: {}",read_len);
+            }
+        }
+        Ok(start_vddr)
+    })
 }
 
 fn sys_openat(dfd: c_int, fname: *const c_char, flags: c_int, mode: api::ctypes::mode_t) -> isize {
